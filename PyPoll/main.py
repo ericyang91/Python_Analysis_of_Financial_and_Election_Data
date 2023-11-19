e359d3c46590e0a0e92f67d829e6c079ac3da19e
ericj <ericjyyang@gmail.com>
2023-11-18 22:04:07 -0500

    finishing up the code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,34 @@
-# Read the CSV file
+# Import modules
 import os
 import csv
 from file_path import path
 
-
+# Define file paths for both reading and writing
 read_path = os.path.join(path, "Resources", "election_data.csv")
 write_path = os.path.join(path, "Analysis", "election_results.txt")
 
-
+# Define the triggering function
 def initial():
-    with open(read_path, 'r') as read_store:
+    with open(read_path, 'r') as read_store: # Read the CSV file
         read_reader = csv.reader(read_store, delimiter=",")
-        header = next(read_reader)
-        total_votes = 0
-        candidates = []
-        counties = []
-        for row in read_reader:
+        header = next(read_reader) # Define header to skip it
+        candidates = [] # For storing the unique candidate names
+        counties = [] # For storing the unique county names
+
+        for row in read_reader: # Iterate over the reader object
             if row[2] not in candidates:
-                candidates.append(row[2])
+                candidates.append(row[2]) # Add every unique candidate name
             if row[1] not in counties:
-                counties.append(row[1])
-            total_votes += 1
+                counties.append(row[1]) # Add every unique county name
+
         build_dict(candidates, counties)
    
-
+# Define a function that builds a Python dictionary
 def build_dict(candidates, counties):
     with open(read_path, 'r') as read_store:
         read_reader = csv.reader(read_store, delimiter=",")
         header = next(read_reader)
-        dict = {county: {candidate: 0 for candidate in candidates} for county in counties}
+        dict = {county: {candidate: 0 for candidate in candidates} for county in counties} # Use nested dictionary comprehension
         for row in read_reader:
             county = row[1]
             candidate = row[2]
@@ -37,16 +37,16 @@ def build_dict(candidates, counties):
 
 
 def calculate_statistics(dict):
-    with open(write_path, "w") as write_store:
-        for county, candidate_votes in dict.items():
+    with open(write_path, "w") as write_store: # Prepare a writing object
+        for county, candidate_votes in dict.items(): # The method dictionary.items() returns iterable key-value pairs. In this case, it iterates over the outer dictionary
             total_votes_by_county = sum(candidate_votes.values())
             write_store.write(f'County: {county}\n')
             write_store.write(f'Total number of votes in the county: {total_votes_by_county}\n')
-            for candidate, votes in candidate_votes.items():
+            for candidate, votes in candidate_votes.items(): # Iterate over the inner dictionary
                 percentage_of_vote = round(votes/total_votes_by_county*100, 2)
                 write_store.write(f'Candidate name: {candidate}\n')
                 write_store.write(f'Candidate number of votes: {votes}\n')
                 write_store.write(f'Candidate vote in percentage: {percentage_of_vote}%\n\n')
 
-
-initial()
+if __name__ == '__main__':
+    initial()
